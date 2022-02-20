baa9fadc5bedb6fef7a840c717fc117430a2f655
unknown <chilamakooru.gangaiiit@gmail.com>
2022-02-21 01:04:21 +0530

    Creating_Contact

---
+++ AddresBook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book
+{
+    internal class AddresBook
+    {
+        List<Contact> contacts=new List<Contact>();
+
+
+        public void AddContact(string name, string address, string city, string state, string zip, string phoneNo, string email)
+        {
+            contacts.Add(new Contact()
+            {
+                name = name,
+                address = address,
+                city = city,
+                state = state,
+                zip = zip,
+                phoneNo = phoneNo,
+                email = email
+            });
+            Console.WriteLine($"Contact of {name} is added");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,9 +7,32 @@ namespace Address_book
 {
     class Program
     {
-        public static void Main(string[] args)
+         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Address book problem");
+            Console.WriteLine("Enter Name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter address");
+            string address = Console.ReadLine();
+
+            Console.WriteLine("Enter city");
+            string city = Console.ReadLine();
+
+            Console.WriteLine("Enter state");
+            string state = Console.ReadLine();
+
+            Console.WriteLine("Enter zip");
+            string zip = Console.ReadLine();
+
+            Console.WriteLine("Enter Phone number");
+            string phoneNO = Console.ReadLine();
+
+            Console.WriteLine("Enter Phone email");
+            string email = Console.ReadLine();
+
+
+           
         }
     }
 }
